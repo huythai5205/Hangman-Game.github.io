dd65559a285900aec40b543fbdb46e4af81918bc
Huy <kennyt995@gmail.com>
2017-11-15 15:05:29 -0700

    creating getGuessed function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,13 @@
 var wins = 0;
 var guessesLeft = 10;
 var guesses = "";
-var aHeroes = ["superman","batman","spiderman","thor","wonder woman","captain america","iron man", "aquaman"];
+var aHeroes = ["superman", "batman", "spiderman", "thor", "wonder woman", "captain america", "iron man", "aquaman"];
 var bStarted = false;
 var hero = "";
 var heroString = "";
 
-document.onkeyup = function (event) {
-    if (!bStarted) {
-        bStarted = true;
-        hero = getHero();
-        heroString = getHeroString(hero);
-        displayGame();
-    } else {
-        var userGuess = event.key;
-
-        if (hero.indexOf(userGuess) >= 0) {
-            heroString.replace(hero.indexOf(userGuess), userGuess);
-            displayGame();
-        } else {
-            guessesLeft--;
-            guesses += userGuess + " ";
-            displayGame();
-        }
-    }
+function replaceLetter(index, character, string) {
+    return string.substr(0, index) + character + string.substr(index + character.length);
 }
 
 function displayGame() {
@@ -33,11 +17,33 @@ function displayGame() {
 function getHeroString(hero) {
     var string = "";
     for (var i = 0; i < hero.length; i++) {
-        string += " -";
+        string += "-";
     }
     return string;
 }
 
-function getHero() {
-    return aHeroes[Math.floor(Math.random() * aHeroes.length)];
+function getGuessed(string) {
+    for (var i = 0; i < string.length; i++){
+        
+    }
+}
+
+document.onkeyup = function (event) {
+    if (!bStarted) {
+        bStarted = true;
+        hero = aHeroes[Math.floor(Math.random() * aHeroes.length)];
+        heroString = getHeroString(hero);
+        displayGame();
+    } else {
+        var userGuess = event.key;
+        var index = hero.indexOf(userGuess);
+        if (index >= 0) {
+            heroString = replaceLetter(index, userGuess, heroString);
+            displayGame();
+        } else {
+            guessesLeft--;
+            guesses += userGuess + " ";
+            displayGame();
+        }
+    }
 }
