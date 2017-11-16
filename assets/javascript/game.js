15f4cb40946b9e9961de64ef1b277a65538877d8
Huy <kennyt995@gmail.com>
2017-11-15 18:49:00 -0700

    need to work on html  and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,45 +5,92 @@ var aHeroes = ["superman", "batman", "spiderman", "thor", "wonder woman", "capta
 var bStarted = false;
 var hero = "";
 var heroString = "";
-
-function replaceLetter(index, character, string) {
-    return string.substr(0, index) + character + string.substr(index + character.length);
-}
+var numLetters = 0;
+var filledLetter = 0;
 
 function displayGame() {
     document.getElementById("game").innerHTML = `<h2>Wins ${wins}</h2><h2>Current Word</h2><h2>${heroString}</h2><h2>Number of Guesses Remaining is: ${guessesLeft}</h2><h2>Letters Already Guessed: ${guesses}</h2>`;
 }
 
-function getHeroString(hero) {
-    var string = "";
-    for (var i = 0; i < hero.length; i++) {
-        string += "-";
-    }
-    return string;
+function newGame() {
+    hero = aHeroes[Math.floor(Math.random() * aHeroes.length)];
+    getHeroString(hero);
+    guessesLeft = 10;
+    guesses = "";
+    filledLetter = 0;
+    displayGame();
 }
 
-function getGuessed(string) {
-    for (var i = 0; i < string.length; i++){
-        
+function getHeroString() {
+    numLetters = 0;
+    heroString = "";
+    for (var i = 0; i < hero.length; i++) {
+        if (hero[i] === " ") {
+            heroString += " ";
+        } else {
+            numLetters++;
+            heroString += "-";
+        }
     }
+}
+
+function isInWord(userGuess) {
+    var bool = false;
+    heroString = heroString.split("");
+    for (var i = 0; i < hero.length; i++) {
+        if (hero[i] === userGuess) {
+            heroString[i] = userGuess;
+            bool = true;
+            filledLetter++;
+        }
+    }
+    heroString = heroString.join("");
+    return bool;
+}
+
+function isGuessed(userGuess) {
+    for (var i = 0; i < guesses.length; i++){
+        if (guesses[i] === userGuess) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function winGame() {
+    wins++;
+    console.log("win");
+}
+
+function lossGame() {
+    console.log("loss");
 }
 
 document.onkeyup = function (event) {
     if (!bStarted) {
         bStarted = true;
-        hero = aHeroes[Math.floor(Math.random() * aHeroes.length)];
-        heroString = getHeroString(hero);
-        displayGame();
+        newGame();
     } else {
         var userGuess = event.key;
-        var index = hero.indexOf(userGuess);
-        if (index >= 0) {
-            heroString = replaceLetter(index, userGuess, heroString);
-            displayGame();
+        if (!isGuessed(userGuess)) {
+            if (isInWord(userGuess)) {
+                guessesLeft--;
+                displayGame();
+                console.log(numLetters + " " + filledLetter);
+                if (numLetters === filledLetter) {
+                    winGame();
+                }
+            } else {
+                guessesLeft--;
+                guesses += userGuess + " ";
+                displayGame();
+                if (guessesLeft === 0) {
+                    lossGame();
+                }
+            }
         } else {
-            guessesLeft--;
-            guesses += userGuess + " ";
-            displayGame();
+            alert(userGuess + " is already guessed");
         }
+
     }
 }
